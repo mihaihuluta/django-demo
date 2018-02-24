cc406a648d669f2d805048b4f4b5e7204d7d12bd
Mihai H <mihai.huluta@gmail.com>
2018-02-24 05:38:16 +0200

    filter reviews per auth user

--- cmafs/apps/reviews/views.py
+++ cmafs/apps/reviews/views.py
@@ -3,9 +3,9 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, \
     IsAuthenticated, SAFE_METHODS
 
-from apps.reviews.serializers import ReviewSerializer
-from apps.reviews.models import Review
-from apps.reviews.permissions import IsOwnerOrReadOnly
+from .serializers import ReviewSerializer
+from .models import Review
+from .permissions import IsOwnerOrReadOnly
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
@@ -13,20 +13,9 @@ class ReviewViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
     serializer_class = ReviewSerializer
 
+    def get_queryset(self):
+        user = self.request.user
+        return Review.objects.filter(user=user.id)
+
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-
-# class UserViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows users to be viewed or edited.
-#     """
-#     queryset = User.objects.all().order_by('-date_joined')
-#     serializer_class = UserSerializer
-
-
-# class GroupViewSet(viewsets.ModelViewSet):
-#     """
-#     API endpoint that allows groups to be viewed or edited.
-#     """
-#     queryset = Group.objects.all()
-#     serializer_class = GroupSerializer
